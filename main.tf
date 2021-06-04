c3ebaff36d41fef08327e3e3a77d8e13b1a80297
Joseph Callen <jcallen@redhat.com>
2021-06-04 14:05:29 -0400

    missing segment_

--- main.tf
+++ main.tf
@@ -13,6 +13,6 @@ module "segments" {
   dhcp_ranges        = ["${var.segment_subnet}.${var.segment_start_number + count.index}.${var.segment_dhcp_start}-${var.segment_subnet}.${var.segment_start_number + count.index}.${var.segment_dhcp_end}"]
   domain_name        = var.segment_domain_name
   dns_servers        = var.segment_dns_servers
-  dhcp_lease_timeout = var.dhcp_lease_timeout
+  dhcp_lease_timeout = var.segment_dhcp_lease_timeout
 }
 
